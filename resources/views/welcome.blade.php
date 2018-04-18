3355bce54530ac95a13d172831b7b95a0d2c33bd
RagilPratama <ragilpratamaaa@gmail.com>
2018-04-18 14:27:34 +0700

    Baru bgt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,7 +77,7 @@
             
             <div class="content">
                 <div class="title m-b-md">
-                    <i>Laragils</i> <b>RagilGanteng</b>
+                    <i>Laragils</i> <b>RagilGantengBanget</b>
                 </div>
             <!--<div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
